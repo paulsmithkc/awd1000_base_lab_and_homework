129e83261dce095c74bc858d9afbb95635092965
Paul Smith <paulsmithkc@gmail.com>
2019-07-29 10:07:27 -0500

    Fixing carousel example

--- murach_js_3e/book_apps/ch09/carousel_animation/carousel.js
+++ murach_js_3e/book_apps/ch09/carousel_animation/carousel.js
@@ -1,42 +1,17 @@
-$(document).ready(function() {
+$(function() {
 		   
-	var slider = $("#image_list");                     // slider = ul element
-	var leftProperty, newleftProperty;
-		
+	const slider = $("#image_list"); // slider = ul element
+	let slideCounter = 0;
+	
 	// the click event handler for the right button						
 	$("#right_button").click(function() { 
-		// get value of current left property
-		leftProperty = parseInt(slider.css("left"));
-		
-		// determine new value of left property
-		if (leftProperty - 300 <= -900) {
-			newLeftProperty = 0;
-		}
-		else {
-			newLeftProperty = leftProperty - 300;
-		}
-		
-		// use the animate function to change the left property
-		slider.animate( {left: newLeftProperty}, 1000);
-	
+		slideCounter = (slideCounter + 1) % 3;
+		slider.animate( {left: slideCounter * -300 }, 1000);
 	});  // end click
 	
 	// the click event handler for the left button
 	$("#left_button").click(function() {
-	
-		// get value of current right property
-		leftProperty = parseInt(slider.css("left"));
-		
-		// determine new value of left property
-		if (leftProperty < 0) {
-			newLeftProperty = leftProperty + 300;
-		}
-		else {
-			newLeftProperty = 0;
-		}
-		
-		// use the animate function to change the left property
-		slider.animate( {left: newLeftProperty}, 1000);				
-
+		slideCounter = (slideCounter - 1 + 3) % 3;
+		slider.animate( {left: slideCounter * -300 }, 1000);
 	});  // end click		
 }); // end ready
